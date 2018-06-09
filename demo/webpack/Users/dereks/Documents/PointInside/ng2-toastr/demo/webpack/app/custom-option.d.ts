f6125604198195e8b8111d69e87c51d8fa1f35b3
Ivan Pankrashin <pankrashin.ivan@gmail.com>
2018-06-10 00:39:17 +0200

    deleting demo

--- demo/webpack/Users/dereks/Documents/PointInside/ng2-toastr/demo/webpack/app/custom-option.d.ts
+++
@@ -1,7 +0,0 @@
-import { ToastOptions } from 'ng2-toastr';
-export declare class CustomOption extends ToastOptions {
-    animate: string;
-    newestOnTop: boolean;
-    showCloseButton: boolean;
-    dismiss: string;
-}
--- demo/webpack/app/boot.ts
+++
@@ -1,9 +0,0 @@
-declare var require;
-import 'reflect-metadata';
-require('zone.js/dist/zone');
-require('zone.js/dist/long-stack-trace-zone');
-
-import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
-import {AppModule} from './app.module';
-
-platformBrowserDynamic().bootstrapModule(AppModule);
